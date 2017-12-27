1c42f8a70d0bf5d17705677a4e92e166c1e75524
Ajai <ajai.np@gmail.com>
2017-12-27 16:14:21 +1100

    color changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { DataService } from './data.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -20,8 +19,7 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    BrowserAnimationsModule
+    FormsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
